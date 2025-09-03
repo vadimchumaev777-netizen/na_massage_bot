8b5adda01861ece72d902fe847d2653011810d01
vadimchumaev777-netizen <vadimchumaev777@gmail.com>
2025-09-03 13:04:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
 
 // Telegram бот
 const token = process.env.TOKEN
-const ADMIN_CHAT_ID = "YOUR_CHAT_ID" // ЗАМЕНИТЕ НА ВАШ CHAT ID!
+const ADMIN_CHAT_ID = "487990672" // ЗАМЕНИТЕ НА ВАШ CHAT ID!
 
 if (!token) {
   console.error('❌ Токен не найден!')
